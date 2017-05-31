74a615ac80d0b06a4c7cc147e2a0559586484da3
RSijelmass <R.Sijelmass@gmail.com>
2017-05-31 12:59:59 +0100

    Create app/model/link.rb - set up DataMapper, included gems datamapper and dm-postgres-adapter - changed name: to title: in feature_test

--- spec/features/list_of_links_spec.rb
+++ spec/features/list_of_links_spec.rb
@@ -1,6 +1,6 @@
 feature 'Seeing list of links' do
   scenario 'user can see list of links, even if it\'s empty' do
-    link.create(url: 'http://google.com', name: 'Google')	  
+    Link.create(url: 'http://google.com', title: 'Google')	  
     visit '/'
     expect(page.status_code).to eq 200
     
